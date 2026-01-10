502769836c07b113a10b2c4948dbcad444ecf1ce
PrakashML <prakashkrishnan08@gmail.com>
2026-01-10 09:51:11 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",   // adjust based on your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
